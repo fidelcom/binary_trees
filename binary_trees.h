01d58f510ec9fdafaa3049b4ac0c07e2d5a76256
fidelcom <okpara2040@gmail.com>
2024-02-28 15:23:53 +0000

    Write a function that goes through a binary tree using post-order traversal

--- binary_trees.h
+++ binary_trees.h
@@ -35,5 +35,6 @@ void binary_tree_print(const binary_tree_t *);
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value);
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value);
 void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int));
+void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int));
 
 #endif
